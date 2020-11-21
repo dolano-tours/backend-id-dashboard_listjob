e11c4533e045aaea3b0386bd3217c6e4558f9732
Titani <bimatitani88@gmail.com>
2020-11-22 02:01:26 +0700

    pekerja cek delete buat fix

--- backend/controller/Pekerja.py
+++ backend/controller/Pekerja.py
@@ -9,4 +9,23 @@ def get_all():
 def get_by_id(id:int):
     
     pekerja = db_session.query(Pekerja).filter_by(id=id)
-    return {"content": [i.to_dict() for i in pekerja]}
+    return {"content": pekerja.to_dict() }
+
+def add(id_employee:int, tipe_pekerja_id:int):
+    pekerja = Pekerja(id_employee=id_employee, tipe_pekerja_id=tipe_pekerja_id)
+    db_session.add(pekerja)
+    db_session.commit()
+    db_session.flush()
+    db_session.refresh(pekerja)
+    return {"content": pekerja.to_dict()}
+
+def delete_by_id(target_id):
+    pekerja = db_session.query(Pekerja).filter_by(id=target_id)
+    data=pekerja.first()
+    pekerja.delete()
+    db_session.commit()
+    db_session.flush()
+    return {"content": data.to_dict()}
+
+
+
--- backend/routes/Pekerja.py
+++ backend/routes/Pekerja.py
@@ -13,3 +13,21 @@ def get_all():
 def get_by_id(id:int):
     pekerja = Pekerja.get_by_id(request.view_args["id"])
     return jsonify(pekerja)
+
+@pekerja_routes.route("/id_employee", methods=['GET'])
+def get_by_id_employee(id_employee:int):
+    pekerja = Pekerja.get_by_nama(request.view_args["id_employee"])
+    return jsonify(pekerja)
+
+@pekerja_routes.route("/add", methods=['POST'])
+def add():
+    id_employee = request.json.get('id_employee')
+    tipe_pekerja_id = request.json.get('tipe_pekerja_id')
+    pekerja = Pekerja.add(id_employee,tipe_pekerja_id)
+    return jsonify(pekerja)
+
+@pekerja_routes.route("/delete", methods=['POST'])
+def delete_by_id():
+    id = request.json.get('id')
+    pekerja = Pekerja.delete_by_id(id)
+    return jsonify(pekerja)
