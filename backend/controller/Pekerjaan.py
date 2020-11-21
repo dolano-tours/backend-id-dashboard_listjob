3da1a38a517cb9d488c3c841b37a34e3d94b5fab
Titani <bimatitani88@gmail.com>
2020-11-22 00:14:15 +0700

    pekerjaan done add dan delete

--- backend/controller/Pekerjaan.py
+++ backend/controller/Pekerjaan.py
@@ -8,5 +8,27 @@ def get_all():
 
 def get_by_id(id:int):
     
-    pekerjaan = db_session.query(Pekerjaan).filter_by(id=id)
-    return {"content": [i.to_dict() for i in pekerjaan]}
+    pekerjaan = db_session.query(Pekerjaan).filter_by(id=id).first()
+    return {"content": pekerjaan.to_dict() }
+
+def get_by_nama(nama:str):
+    pekerjaan = db_session.query(Pekerjaan).filter_by(nama=nama).first()
+    return {"content": pekerjaan.to_dict()}
+
+def add(nama:str,pekerja_id:int,pemberi_tugas_id:int):
+    pekerjaan = Pekerjaan(nama=nama, pekerja_id=pekerja_id, pemberi_tugas_id=pemberi_tugas_id, status="revisi")
+    db_session.add(pekerjaan)
+    db_session.commit()
+    db_session.flush()
+    db_session.refresh(pekerjaan)
+    return {"content": pekerjaan.to_dict()}
+
+def delete_by_id(target_id):
+    pekerjaan = db_session.query(Pekerjaan).filter_by(id=target_id)
+    data=pekerjaan.first()
+    pekerjaan.delete()
+    db_session.commit()
+    db_session.flush()
+    return {"content": data.to_dict()}
+
+
--- backend/routes/Pekerjaan.py
+++ backend/routes/Pekerjaan.py
@@ -13,3 +13,24 @@ def get_all():
 def get_by_id(id:int):
     pekerjaan = Pekerjaan.get_by_id(request.view_args["id"])
     return jsonify(pekerjaan)
+
+@pekerjaan_routes.route("/nama/<nama>", methods=['GET'])
+def get_by_nama(nama:str):
+    pekerjaan=Pekerjaan.get_by_nama(request.view_args["nama"])
+    return jsonify(pekerjaan)
+
+@pekerjaan_routes.route("/add", methods=['POST'])
+def add():
+    nama = request.json.get('nama')
+    pekerja_id = request.json.get('pekerja_id')
+    pemberi_tugas_id = request.json.get('pemberi_tugas_id')
+    pekerjaan = Pekerjaan.add(nama,pekerja_id,pemberi_tugas_id)
+    return jsonify(pekerjaan)
+
+@pekerjaan_routes.route("/delete", methods=['POST'])
+def delete_by_id():
+    id = request.json.get('id')
+    pekerjaan = Pekerjaan.delete_by_id(id)
+    return jsonify(pekerjaan)
+
+
