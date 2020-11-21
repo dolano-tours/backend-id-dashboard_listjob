a39015c693d9c72850f7078c5d675525cb0140a4
Titani <bimatitani88@gmail.com>
2020-11-21 13:11:37 +0700

    CRUD tipe pekerja

--- backend/controller/TipePekerja.py
+++ backend/controller/TipePekerja.py
@@ -8,5 +8,48 @@ def get_all():
 
 def get_by_id(id:int):
     
-    tipe_pekerja = db_session.query(TipePekerja).filter_by(id=id)
-    return {"content": [i.to_dict() for i in tipe_pekerja]}
+    tipe_pekerja = db_session.query(TipePekerja).filter_by(id=id).first()
+    return {"content": tipe_pekerja.to_dict()}
+
+def get_by_nama(nama:str):
+    
+    tipe_pekerja = db_session.query(TipePekerja).filter_by(nama=nama).first()
+    return {"content": tipe_pekerja.to_dict()}
+
+def add(nama:str):
+    tipe_pekerja = TipePekerja(nama=nama)
+    db_session.add(tipe_pekerja)
+    db_session.commit()
+    db_session.flush()
+    db_session.refresh(tipe_pekerja)
+
+    return {"content": tipe_pekerja.to_dict()}
+
+def update_by_id(target_id,nama):
+    tipe_pekerja = db_session.query(TipePekerja).filter_by(id=target_id)
+    tipe_pekerja.update(
+       { TipePekerja.nama:nama}
+    )
+    db_session.commit()
+    db_session.flush()
+    data = db_session.query(TipePekerja).filter_by(id=target_id).first()
+    return{"content": data.to_dict()}
+
+def update_by_nama(target_nama,nama):
+    tipe_pekerja = db_session.query(TipePekerja).filter_by(nama=target_nama)
+    tipe_pekerja.update(
+        {TipePekerja.nama:nama}
+    )
+    db_session.commit()
+    db_session.flush()
+    data = db_session.query(TipePekerja).filter_by(nama=nama).first()
+    return{"content": data.to_dict()}
+
+def delete_by_id(target_id):
+    tipe_pekerja = db_session.query(TipePekerja).filter_by(id=target_id)
+    data=tipe_pekerja.first()
+    tipe_pekerja.delete()
+    db_session.commit()
+    db_session.flush()
+    return{"content": data.to_dict()}
+
--- backend/routes/TipePekerja.py
+++ backend/routes/TipePekerja.py
@@ -13,3 +13,37 @@ def get_all():
 def get_by_id(id:int):
     tipe_pekerja = TipePekerja.get_by_id(request.view_args["id"])
     return jsonify(tipe_pekerja)
+
+@tipe_pekerja_routes.route("/nama/<nama>", methods=['GET'])
+def get_by_nama(nama:str):
+    tipe_pekerja = TipePekerja.get_by_nama(request.view_args["nama"])
+    return jsonify(tipe_pekerja)
+
+@tipe_pekerja_routes.route("/add", methods=['POST'])
+def add():
+    nama = request.json.get('nama')
+    tipe_pekerja = TipePekerja.add(nama)
+
+    return jsonify(tipe_pekerja)
+
+@tipe_pekerja_routes.route("/update/id", methods=['PUT'])
+def update_by_id():
+    id  = request.json.get('id')
+    nama = request.json.get('nama')
+    tipe_pekerja = TipePekerja.update_by_id(id,nama)
+    return jsonify(tipe_pekerja)
+
+@tipe_pekerja_routes.route("/update/nama", methods=['PUT'])
+def update_by_nama():
+    target_nama = request.json.get('get_nama')
+    nama = request.json.get('nama') 
+    tipe_pekerja = TipePekerja.update_by_nama(target_nama,nama)
+    return jsonify(tipe_pekerja)
+
+
+
+@tipe_pekerja_routes.route("/delete", methods=['DELETE'])
+def delete_by_id():
+    id = request.json.get('id')
+    tipe_pekerja = TipePekerja.delete_by_id(id)
+    return jsonify(tipe_pekerja)
